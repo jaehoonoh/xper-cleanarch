585e35a34fe30396409586a3c8112047d381aed2
Martin Oh <martin@lambda256.io>
2020-06-01 09:01:36 +0900

    Add Unit Test for LoginController

--- test/unit/layer/controller/LoginController.spec.ts
+++ test/unit/layer/controller/LoginController.spec.ts
@@ -1,7 +1,7 @@
 import {LoginController} from "../../../../src/layer/controller/LoginController";
 import {UserService} from "../../../../src/layer/application/UserService";
 import * as sinon from 'sinon';
-import {NoSuchUserException} from "../../../../src/layer/domain/AuthenticationError";
+import {NoSuchUserException, PasswordIncorrectException} from "../../../../src/layer/domain/AuthenticationError";
 import {UserRepository} from "../../../../src/layer/domain/UserRepository";
 import * as assert from "assert";
 
@@ -9,29 +9,65 @@ import * as assert from "assert";
 describe('LoginController', function () {
     describe('login', function () {
         it('should return [404,No Such User] for non-existing-user when userService throws NoSuchUserException', function () {
-
-            let userService = {
-                authenticate: function() {
-                    throw new NoSuchUserException("");
-                }
-            };
-
+            let userService = { authenticate: function() { throw new NoSuchUserException(""); } };
             let loginController = new LoginController(userService as unknown as UserService);
 
             let notRegisteredUsername = "NOT_REGISTERED";
-            let req = { body: { username: notRegisteredUsername } };
-            let res = {
-                s: 0,
-                obj: { message: '' },
-                status: function(s) { this.s = s; },
-                json: function(obj) { console.log('json'); this.obj = obj; },
-                getStatus: function() { return this.s; }
-            }
+            let req = makeFakeRequest(notRegisteredUsername);
+            let res = makeFakeResponse();
 
             loginController.login(req, res);
 
             assert.strictEqual(res.getStatus(), 404);
             assert.strictEqual(res.obj.message, "Not Registered : " + notRegisteredUsername);
         })
+
+        it('should return [400,Incorrect Password] for Incorrect Password Login', function () {
+
+            let userService = { authenticate: function() { throw new PasswordIncorrectException(""); } };
+            let loginController = new LoginController(userService as unknown as UserService);
+
+            let req = makeFakeRequest("");
+            let res = makeFakeResponse();
+
+            loginController.login(req, res);
+
+            assert.strictEqual(res.getStatus(), 400);
+            assert.strictEqual(res.obj.message, "Incorrect Password");
+        })
+
+        it('should return [200,Succeed] when Login succeeds', function () {
+            let userService = { authenticate: function() { return true; } };
+            let loginController = new LoginController(userService as unknown as UserService);
+
+            let req = makeFakeRequest("");
+            let res = makeFakeResponse()
+
+            loginController.login(req, res);
+
+            assert.strictEqual(res.getStatus(), 200);
+            assert.strictEqual(res.obj.message, "Succeed");
+        })
+
+        function makeFakeResponse() {
+            return {
+                s: 0,
+                obj: {message: ''},
+                status: function (s) {
+                    this.s = s;
+                },
+                json: function (obj) {
+                    console.log('json');
+                    this.obj = obj;
+                },
+                getStatus: function () {
+                    return this.s;
+                }
+            };
+        }
+
+        function makeFakeRequest(notRegisteredUsername: string) {
+            return {body: {username: notRegisteredUsername}};
+        }
     })
 })
